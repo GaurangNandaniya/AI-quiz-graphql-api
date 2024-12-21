0537e425244edc5830ca221b560e0143e2fc04f8
Gaurang Nandaniya <gaurang1234522@gmail.com>
2024-12-21 17:23:34 +0530

    feat: add mutation and type resolvers

--- src/graphql/resolvers/mutationResolvers.js
+++ src/graphql/resolvers/mutationResolvers.js
@@ -1,5 +1,15 @@
+import { generateQuiz } from "Controllers/quizGeneration";
+
 export const mutation = {
   Mutation: {
-    createQuiz: () => {},
+    createQuiz: async (parentArgs, args, context) => {
+      const { input } = args;
+
+      const result = await generateQuiz({
+        ...input,
+        jwtUser: context?.expressResponse.locals.jwtUser,
+      });
+      return result;
+    },
   },
 };
--- src/graphql/resolvers/typeResolvers.js
+++ src/graphql/resolvers/typeResolvers.js
@@ -1,8 +1,31 @@
-export const typeResolvers = {
+import _ from "lodash";
+
+export const difficultyLevelResolvers = {
   User: {
     id: ({ id }) => id,
     firstName: ({ first_name }) => first_name,
     lastName: ({ last_name }) => last_name,
     email: ({ email }) => email,
+  },
+  Quiz: {
+    id: ({ id }) => id,
+    questionCount: ({ questionCount }) => questionCount,
+    title: ({ title }) => title,
+    type: ({ type }) => type,
+    difficultyLevel: ({ difficultyLevel }) => difficultyLevel,
+    time: ({ time }) => time,
+    questions: ({ questions, questionId }) => {
+      if (_.isEmpty(questionId)) {
+        return questions;
+      }
+      //TODO: add else case with dataloader to fetch question by ids
+    },
+  },
+  Question: {
+    id: ({ id }) => id,
+    text: ({ text }) => text,
+    options: ({ options }) => options,
+    type: ({ type }) => type,
+    answer: ({ answer }) => answer,
   },
 };
