dec0e4fb9ccbf43c54fd39949ef70fcf23094b3d
Gaurang Nandaniya <gaurang1234522@gmail.com>
2024-12-21 17:08:07 +0530

    feat: add type definition for Quiz and Questions

--- src/graphql/typeDefs/index.js
+++ src/graphql/typeDefs/index.js
@@ -6,18 +6,57 @@ type User {
   email: String
 }
 
-type Quiz{
-    id:ID!
+type Question {
+  id:ID!
+  text:String
+  options:JSON
+  type:QUESTION_TYPE
+  answer: JSON
+}
 
+type Quiz {
+    id:ID!
+    questionCount: Int
+    title: String
+    type: QUIZ_TYPE
+    difficultyLevel: DIFFICULTY_LEVEL_ENUM
+    time: JSON
+    questions: [Question!]
+}
+
+enum QUESTION_TYPE {
+  MULTIPLE_CHOICE
+  TEXT_BASED
+  MULTIPLE_CHOICE_MULTI_ANSWER
+}
+
+enum DIFFICULTY_LEVEL_ENUM {
+    EASY
+    MEDIUM
+    HARD
+    ADVANCE
+}
+
+enum QUIZ_TYPE {
+  NON_TIME_BASED
+  TIME_BASED
+}
+
+input CreateQuizInput{
+    description: String
+    isTimeBased: Boolean
+    difficultyLevel: DIFFICULTY_LEVEL_ENUM
+    noOfQuestions: Int
 }
 
 type Query {
     user(id:ID!):User
 
+
   }
 
   type Mutation{
-    createQuiz:Quiz
+    createQuiz(input:CreateQuizInput):Quiz
   }
 
 `;
