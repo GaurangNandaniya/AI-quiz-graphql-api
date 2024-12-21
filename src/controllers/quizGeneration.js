31c8db804c646cf74f8c6985fed9016f3f4f919f
Gaurang Nandaniya <gaurang1234522@gmail.com>
2024-12-21 17:20:12 +0530

    feat: add controller for quiz generation

---
+++ src/controllers/quizGeneration.js
@@ -0,0 +1,101 @@
+import _ from "lodash";
+import { getPromptByAction } from "./promptsController";
+import { replacePlaceholders } from "Utils/stringUtils";
+import { generateAIResponse } from "./geminiAIController";
+import { insertQuizDetails } from "./quizzesController";
+import { insertUserQuizMap } from "./userQuizMapController";
+import { insertQuizQuestions } from "./quizQuestionControllers";
+
+export const generateQuiz = async (data) => {
+  const { description, isTimeBased, difficultyLevel, noOfQuestions, jwtUser } =
+    data;
+  const { userId } = jwtUser;
+
+  //getting prompts for quiz creation
+  const prompts = await getPromptByAction({
+    actions: ["SYSTEM_PROMPT_QUIZ_CREATION", "USER_PROMPT_QUIZ_CREATION"],
+  });
+
+  const systemPrompt = _.find(
+    prompts,
+    (item) => item.action == "SYSTEM_PROMPT_QUIZ_CREATION"
+  ).prompt;
+  let userPrompt = _.find(
+    prompts,
+    (item) => item.action == "USER_PROMPT_QUIZ_CREATION"
+  ).prompt;
+
+  //injecting user input into prompts
+  userPrompt = replacePlaceholders({
+    string: userPrompt,
+    data: {
+      description,
+      isTimeBased: isTimeBased ? "YES" : "NO",
+      difficultyLevel,
+      noOfQuestions,
+    },
+    placeholderText: "can ignore",
+  });
+
+  //getting AI response
+  const aiResponse = await generateAIResponse({
+    systemPrompt,
+    prompt: userPrompt,
+  });
+
+  const {
+    quizType,
+    numberOfQuestions,
+    timeForQuiz,
+    difficultyLevel: quizDifficultyLevel,
+    title,
+    questions,
+    answers,
+  } = aiResponse;
+
+  //insert quiz info to respective tables
+  //#1
+  const quizInfo = await insertQuizDetails({
+    quizDetails: {
+      title,
+      type: quizType,
+      questionCount: numberOfQuestions,
+      time: timeForQuiz,
+      difficultyLevel: quizDifficultyLevel,
+    },
+  });
+
+  const { id: quizId } = quizInfo;
+
+  const promiseArray = [];
+  //#2
+  promiseArray.push(insertUserQuizMap({ userId, quizId, isOwner: true }));
+  //#3
+  promiseArray.push(
+    insertQuizQuestions({
+      questions: _.map(questions, (question, index) => ({
+        ...question,
+        answer: answers[index].value,
+      })),
+      quizId,
+    })
+  );
+
+  const [, quizQuestions] = await Promise.allSettled(promiseArray);
+
+  return {
+    id: quizId,
+    questionCount: numberOfQuestions,
+    title,
+    type: quizType,
+    difficultyLevel: quizDifficultyLevel,
+    time: timeForQuiz,
+    questions: _.map(quizQuestions, (question) => ({
+      id: question.id,
+      text: question.question_text,
+      options: question.options,
+      type: question.type,
+      answer: question.answer,
+    })),
+  };
+};
